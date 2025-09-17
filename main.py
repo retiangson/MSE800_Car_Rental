09cffd7281787b155306ca1def377e0e5c4e5ccf
retiangson <116651559+retiangson@users.noreply.github.com>
2025-09-18 01:07:21 +1200

    fixed the rename case

--- main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-from Business.Services.Installer import ServiceInstaller
-from ui.MainMenu import run
-
-if __name__ == "__main__":
-    print("Starting Car Rental System.........")
-    installer = ServiceInstaller()
-    run(installer)   # pass installer into run
